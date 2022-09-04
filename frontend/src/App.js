d802da15e78ba5a5349b22d2b26aee0301007474
Nguyen Vu <nguyenvu191000@gmail.com>
2022-09-04 21:30:30 +0700

    update

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import Home from "./components/Home";
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="container container-fluid">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="py-1">
+        <p className="text-center  mt-1">Shopping Cart</p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
