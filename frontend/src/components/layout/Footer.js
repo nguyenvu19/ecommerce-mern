2632ba400b73345267181f98113fcbe308ec9029
Nguyen Vu <nguyenvu191000@gmail.com>
2022-09-05 15:17:23 +0700

    update

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { Fragment } from 'react'
 
 const Footer = () => {
-  return (
-    <>
-      <footer className="py-1">
-        <p className="text-center  mt-1">Shopping Cart</p>
-      </footer>
-    </>
-  );
-};
+    return (
+        <Fragment>
+            <footer className="py-1">
+                <p className="text-center mt-1">
+                    Shopping Cart - 2019-2020, All Rights Reserved
+                </p>
+            </footer>
+        </Fragment>
+    )
+}
 
-export default Footer;
+export default Footer
