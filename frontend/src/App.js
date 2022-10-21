b9c7582b42dac58bcbb8ac3e1ddfb44b74bfd306
Nguyen Vu <nguyenvu191000@gmail.com>
2022-10-21 10:46:24 +0700

    footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer(props) {
+  return (
+    <>
+      <footer class="py-1">
+        <p class="text-center text-white mt-1">Ecommerce Shop</p>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
