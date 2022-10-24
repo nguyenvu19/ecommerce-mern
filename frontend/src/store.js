a8a7cec9877f8a90892321fb1719e5ebf46977b5
Nguyen Vu <nguyenvu191000@gmail.com>
2022-10-24 10:19:17 +0700

    store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,3 +3,14 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 const reducer = combineReducers({});
+
+let initialState = {};
+
+const middleware = [thunk];
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(middleware))
+);
+
+export default store;
