3c500d794d63cbc4d9246a0c3e9f065b13c93607
Nguyen Vu <nguyenvu191000@gmail.com>
2022-10-27 16:42:04 +0700

    register

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -3,14 +3,19 @@ import {
   LOGIN_FAIL,
   LOGIN_REQUEST,
   LOGIN_SUCCESS,
+  REGISTER_USER_REQUEST,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_FAIL,
 } from "../constants/userConstants";
 
 export const authReducer = (state = { user: {} }, action) => {
   switch (action.type) {
     case LOGIN_REQUEST:
+    case REGISTER_USER_REQUEST:
       return { loading: false, isAuthenticated: false };
 
     case LOGIN_SUCCESS:
+    case REGISTER_USER_SUCCESS:
       return {
         ...state,
         loading: false,
@@ -19,6 +24,7 @@ export const authReducer = (state = { user: {} }, action) => {
       };
 
     case LOGIN_FAIL:
+    case REGISTER_USER_FAIL:
       return {
         ...state,
         loading: false,
